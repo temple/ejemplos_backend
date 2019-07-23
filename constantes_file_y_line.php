60b5e21c9c623c9fa49d47aceb4dc51e26320dee
Xavier Subirà <el.temple@gmail.com>
2019-07-23 20:32:13 +0200

    Las constantes del lenguaje __LINE__ y __FILE__

---
+++ constantes_file_y_line.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "ESTOY EN EL ARCHIVO ".__FILE__.", EN LA LÍNEA ".__LINE__;
+echo "ESTOY EN EL ARCHIVO ".__FILE__.", EN LA LÍNEA ".__LINE__;
+echo "ESTOY EN EL ARCHIVO ".__FILE__.", EN LA LÍNEA ".__LINE__;
