07fc25fa7517ccd8be9d315d2c4b6fa8a9ab6e36
Xavier Subirà <el.temple@gmail.com>
2019-07-23 20:35:47 +0200

    La constante __DIR__ en php dice la ruta hacia la carpeta donde hay un archivo.

---
+++ constante_DIR_.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "ESTOY EN EL ARCHIVO ".__FILE__.", EN LA LÍNEA ".__LINE__."\n";
+echo "ESTOY EN EL ARCHIVO ".__FILE__.", EN LA LÍNEA ".__LINE__."\n";
+echo "ESTOY EN EL ARCHIVO ".__FILE__.", EN LA LÍNEA ".__LINE__."\n";
+
+echo "ESTE ARCHIVO ESTÁ EN LA CARPETA ".__DIR__."\n";
