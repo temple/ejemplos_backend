72f5b0cfa93ac362c784be583407e5038c4530ff
Xavier Subirà <el.temple@gmail.com>
2019-07-24 16:33:14 +0200

    Cómo se crean strings en PHP? (v1)

---
+++ strings_maneras_de_escribirlos.php
@@ -0,0 +1,11 @@
+<?php
+
+// Simple (usando ' o ")
+$var = "cadena";
+$var2 = 'de texto';
+
+// Con concatenaciones
+$var3 ="Las variables \"\$var\" y \"\$var2\" valen ".$var." y ".$var2;
+
+// Con referencias
+$var4 = "Las variables \"\$var\" y \"\$var2\" valen $var y $var2";
