e3c1ce0b1f153bb8e412a2b50e410dadc3f8dc85
Xavier Subirà <el.temple@gmail.com>
2019-07-25 15:39:40 +0200

    Al ejemplo anterior le hemos añadido la operación de "añadir al final del array", que se hace con $array[]  = valor

--- arrays_añadiendo_un_valor_sin_clave.php
+++ arrays_añadiendo_un_valor_sin_clave.php
@@ -7,3 +7,10 @@ $matriz = array("foo" => "bar", 12 => true, false);
 // continuará a partir de 12, por lo tanto será 13
 
 print_r($matriz);
+
+// Veamos ahora como se añadiría un valor "false" a un array como el anterior, pero de solo 2 posiciones
+// los print_r daran el mismo resultado
+$matriz = array("foo" => "bar", 12 => true);
+$matriz[] = false;
+
+print_r($matriz);
