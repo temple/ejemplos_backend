50fbdecaf1292d26122d6b424cc6b8dd59597770
Xavier Subirà <el.temple@gmail.com>
2019-07-25 18:02:17 +0200

    SOLUCIÓN Ejercicio: Modifica las propiedades del array resultante del typecasting de un objeto, que era el resultado de un typecasting anterior de un array, siguiendo las indicaciones a modo de comentarios

--- ejercicio_dos_typecasting_entre_array_y_objeto.php
+++ ejercicio_dos_typecasting_entre_array_y_objeto.php
@@ -7,7 +7,7 @@
   $array = array(
   			'clave' =>  "valor",
   			'a' => false,
-  			'b' => true ,
+  			'b' => ???? ,
   			'ultima' => 0
   			 );
 
@@ -15,10 +15,20 @@
 
   $array_resultante = (array) $objeto;
 
-  // Haz que la posicion a del array valga la propiedad b del objeto
+  // Haz que la posicion "a" del array valga la propiedad "b" del objeto
+  $array_resultante["a"] = $objeto->b;
 
-  // Haz que la posición b del array valga la propiedad ultima del objeto
+  // Haz que la posición "b" del array valga la propiedad "ultima" del objeto
+  $array_resultante["b"] = $objeto->ultima;
 
-  // Haz que la posición ultima del array valga la propiedad clave del objeto
+  // Haz que la posición "ultima" del array valga la propiedad "clave" del objeto
+  $array_resultante["ultima"] = $objeto->clave;
 
-  // Haz que la posición clave del array valga la propiedad a del objeto
+  // Haz que la posición "clave" del array valga la propiedad a del objeto
+  $array_resultante["clave"] = $objeto->a;
+
+
+
+
+
+
