913bfe9d3c88245780b588be2fa6aa6b6a82e7d7
Xavier Subirà <el.temple@gmail.com>
2019-07-25 18:20:31 +0200

    Operadores aritméticos en PHP

---
+++ operadores_aritméticos.php
@@ -0,0 +1,25 @@
+<?php 
+
+$a + $b // suma
+$a - $b // resta
+$a * $b // multiplicar
+$a / $b // división de $a entre $b
+$a % $b // resto de la división (módulo) de $a entre $b
+-$a // negativo de $a
+$a**$b // $a elevada a $b ($a power $b)
+
+
+
+$a = $a + $b;
+$a += $b;
+
+$a = $a - $b;
+$a -= $b;
+
+$a *= $b;
+
+$a /= $b;
+
+$a %= $b;
+
+$a **= $b;
