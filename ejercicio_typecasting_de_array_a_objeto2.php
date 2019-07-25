c97673e52367d4b9ee3f2799d7744ab27f23094d
Xavier Subirà <el.temple@gmail.com>
2019-07-25 16:44:19 +0200

    Ejercicio: Modifica las propiedades del objeto resultante del typecasting de un array hacia objeto, pero sin usar los valores nuevos de manera directa, sinó usando para ello el array original

---
+++ ejercicio_typecasting_de_array_a_objeto2.php
@@ -0,0 +1,25 @@
+<?php
+/**
+  Ejercicio, modifica la propiedad de un objeto resultante de un typecast a objeto de un array
+  */
+
+  $array = array(
+  			'clave' =>  "valor",
+  			'a' => false,
+  			'b' => true ,
+  			'ultima' => 0
+  			 );
+
+  $objeto = (object) $array;
+
+
+  // Haz que la propiedad del objeto a valga true y que la propiedad b valga false
+
+  BORRAR ESTAS LÍNEAS: No puedes usar los valores directamente, debes usar $array 
+  para cambiar los valores de $objeto
+
+  // Haz que la propiedad ultima valga valor y que la propiedad clave valga 0 
+
+  BORRAR ESTAS LÍNEAS: No puedes usar los valores directamente, debes usar $array 
+  para cambiar los valores de $objeto
+  
