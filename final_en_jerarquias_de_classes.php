b4550db53264f62bb0b35888383013b48069e132
Xavier Subirà <el.temple@gmail.com>
2019-07-29 19:30:32 +0200

    Uso de final en clases que no queremos que sean extendidas

---
+++ final_en_jerarquias_de_classes.php
@@ -0,0 +1,13 @@
+<?php
+
+final class ultima_de_su_especie{}
+
+/*
+class nueva_ultima extends ultima_de_su_especie{
+
+}
+
+No podemos extender una clase final, la palabra lo dice.
+Si descomentamos aparece el error
+PHP Fatal error:  Class nueva_ultima may not inherit from final class (ultima_de_su_especie) in /var/www/ejemplos/final_en_jerarquias_de_classes.php on line 7
+*/
