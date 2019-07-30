e8a9eaad7ec9489e51b526e41ad8394f4c821a37
Xavier Subirà <el.temple@gmail.com>
2019-07-30 18:34:37 +0200

    Interfaces y herencia Las interfaces pueden heredar de otras interfaces (aunque no es aconsejable porque vulnera el principio Interface Segreggation de los SOLID Principles).
    Cuando una interfaz extiende a otra interfaz, los requisitos establecidos en la interfaz madre, pasan a ser exigidos por la interfaz hija.
    
    En este ejemplo vemos que la interfaz LineCollectionInterface tiene dos requisitos a modo de funciones getLines y mergeLine.
    A estos dos requisitos le tendremos que sumar los requisitos proveninentes de la interfaz Countable (que es propia de PHP), y que consiste en disponer de una función (pública) llamada count que no recibe parámetros pero que devuelve un integer.
    
    Para más info sobre \Countable visitar https://php.net/manual/es/class.countable.php

---
+++ interfaces_y_herencia.php
@@ -0,0 +1,29 @@
+<?php
+
+class Line{
+    public $quantity;
+    public $product;
+    public $discount;
+
+}
+
+interface LineCollectionInterface
+    extends \Countable
+{
+    /**
+     * Gets the Line Collection as an array of lines
+     * 
+     * @return array 
+     *          es: La LineCollection como un array no asociativo de sus instancias de Line
+     *          en: The LineCollection as a non-associative array of its Line instances
+     */
+    public function getLines() : array;
+    /**
+     * Merges a line into the lines collection
+     * @param Line $line es: La línea de compra a integrar en la colección
+     *                   en: The cart Line to be merged into the collection
+     * @return $this
+     */
+    public function mergeLine(Line $line) : LineCollectionInterface;
+    
+}
